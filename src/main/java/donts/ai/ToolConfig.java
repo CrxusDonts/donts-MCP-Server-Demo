e7b5467dde91a328f246b05a3ec214378754615d
dongjiyu <djy1291923247@foxmail.com>
2025-03-23 12:43:14 +0800

    init

---
+++ src/main/java/donts/ai/ToolConfig.java
@@ -0,0 +1,22 @@
+package donts.ai;
+
+import donts.ai.calculator.CalculatorMcpServer;
+import donts.ai.weather.WeatherMcpServer;
+import org.springframework.ai.tool.ToolCallbackProvider;
+import org.springframework.ai.tool.method.MethodToolCallbackProvider;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ToolConfig {
+    @Bean
+    public ToolCallbackProvider weatherTools(WeatherMcpServer weatherMcpServer) {
+        return MethodToolCallbackProvider.builder().toolObjects(weatherMcpServer).build();
+    }
+
+    @Bean
+    public ToolCallbackProvider calculatorTools(CalculatorMcpServer calculatorMcpServer) {
+        return MethodToolCallbackProvider.builder().toolObjects(calculatorMcpServer).build();
+    }
+
+}
